b4da9bfc66b816d3d44e4f2525d6b463e6d1ecd8
Franmc027 <123466051+Franmc027@users.noreply.github.com>
2023-04-20 10:10:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,24 +19,19 @@ def process():
     # Save img in /static/cars folder
     file = request.files['img']
     filename = secure_filename(file.filename)
-
     path = 'C:\\Users\\losfr\\PycharmProjects\\Matriculas\\static\\cars'
     file.save(os.path.join(path, filename))
 
     img = path + '\\' + filename
-    print(img)
     request_url = cf.OPENALPR_CLOUD_API_URL + '&secret_key=' + cf.TOKEN
     # Files that contain the images to be analysed
     files = {'image': open(img, 'rb')}
 
-    # Actual OpenALPR request
+    # OpenALPR request
     data = requests.post(request_url, files=files);
 
     # Analyse the obtained results
     plate, brand, model, color = cf.analyse_results(data.json())
-
-    print(plate, brand, model, color)
-
     return render_template('car_data.html', img=filename, plate=plate, brand=brand, model=model, color=color)
 
 
